a9697600b97ed6c547e33cb5b133d4bf42875c32
Wilson Lin <code@wilsonl.in>
2022-03-08 09:39:32 +0000

    Add read to Rust script

--- script/rust/src/main.rs
+++ script/rust/src/main.rs
@@ -61,6 +61,18 @@ fn args_create_object(key: &[u8], size: u64) -> Vec<u8> {
   return payload;
 }
 
+fn args_read_object(key: &[u8], start: u64, end: u64) -> Vec<u8> {
+  let raw_len = 1 + key.len() + 8 + 8;
+  let mut payload = Vec::<u8>::with_capacity(1 + 1 + raw_len);
+  payload.push(1);
+  payload.push(raw_len as u8);
+  payload.push(key.len() as u8);
+  payload.extend_from_slice(key);
+  payload.extend_from_slice(&start.to_be_bytes());
+  payload.extend_from_slice(&end.to_be_bytes());
+  return payload;
+}
+
 fn args_write_object(key: &[u8], obj_no: u64, start: u64) -> Vec<u8> {
   let raw_len = 1 + key.len() + 8 + 8;
   let mut payload = Vec::<u8>::with_capacity(1 + 1 + raw_len);
@@ -78,6 +90,12 @@ fn args_write_object(key: &[u8], obj_no: u64, start: u64) -> Vec<u8> {
 struct Cli {
   #[clap(long)]
   concurrency: usize,
+
+  #[clap(long)]
+  upload: bool,
+
+  #[clap(long)]
+  read: bool,
 
   #[clap(long)]
   count: usize,
@@ -103,7 +121,7 @@ struct WorkerState {
   data: Vec<u8>,
 }
 
-async fn worker(worker_state: Arc<WorkerState>) -> Result<(), Box<dyn Error>> {
+async fn upload_worker(worker_state: Arc<WorkerState>) -> Result<(), Box<dyn Error>> {
   let mut conn_wrk = UnixStream::connect("/tmp/turbostore.sock").await?;
   let mut conn_mgr = UnixStream::connect("/tmp/turbostore-manager.sock").await?;
 
@@ -143,6 +161,34 @@ async fn worker(worker_state: Arc<WorkerState>) -> Result<(), Box<dyn Error>> {
   Ok(())
 }
 
+async fn read_worker(worker_state: Arc<WorkerState>) -> Result<(), Box<dyn Error>> {
+  let mut conn_wrk = UnixStream::connect("/tmp/turbostore.sock").await?;
+
+  loop {
+    let no = worker_state.next.fetch_add(1, Ordering::Relaxed);
+    if no >= worker_state.total {
+      break;
+    };
+    let key = format!("/random/data/{}", no);
+    let key = key.as_bytes();
+
+    // read_object.
+    keep_writing(&mut conn_wrk, &args_read_object(key, 0, 0)).await?;
+    let read_res_raw = keep_reading(&mut conn_wrk, 17).await?;
+    if read_res_raw[0] != 0 {
+      return Err(Box::new(TurbostoreError::from_primitive(read_res_raw[0])));
+    };
+    let actual_start = read_u64(&read_res_raw[1..]);
+    let actual_len = read_u64(&read_res_raw[9..]);
+    let read_data = keep_reading(&mut conn_wrk, actual_len as usize).await?;
+    if actual_start != 0 || read_data != worker_state.data {
+      panic!("Invalid data");
+    };
+  };
+
+  Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   let args = Cli::parse();
@@ -155,16 +201,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
     data: data.into_bytes(),
     total: args.count,
   });
-  let mut workers = vec![];
-  for _ in 0..args.concurrency {
-    workers.push(worker(worker_state.clone()));
+  let started = if args.read {
+    if args.upload {
+      panic!("Invalid mode argument");
+    };
+    let mut workers = vec![];
+    for _ in 0..args.concurrency {
+      let worker_state = worker_state.clone();
+      workers.push(
+        tokio::spawn(async move {
+          upload_worker(worker_state).await.expect("failed");
+        })
+      );
+    };
+    let started = Instant::now();
+    future::try_join_all(workers).await?;
+    started
+  } else {
+    if !args.upload {
+      panic!("Invalid mode argument");
+    };
+    let mut workers = vec![];
+    for _ in 0..args.concurrency {
+      let worker_state = worker_state.clone();
+      workers.push(
+        tokio::spawn(async move {
+          read_worker(worker_state).await.expect("failed");
+        })
+      );
+    };
+    let started = Instant::now();
+    future::try_join_all(workers).await?;
+    started
   };
-  let started = Instant::now();
-  future::try_join_all(workers).await?;
 
   let elapsed = started.elapsed().as_secs_f64();
   println!("Effective time: {} seconds", elapsed);
-  println!("Effective processing rate: {} seconds", args.count as f64 / elapsed);
+  println!("Effective processing rate: {} per second", args.count as f64 / elapsed);
   println!("Effective bandwidth: {} MiB/s", (args.size * args.count) as f64 / 1024.0 / 1024.0 / elapsed);
 
   Ok(())
