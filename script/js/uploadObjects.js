b9f8b893e4412b9b3149f276124f85e804b0e361
Wilson Lin <code@wilsonl.in>
2022-03-16 13:30:02 +0000

    Update script

--- script/js/uploadObjects.js
+++ script/js/uploadObjects.js
@@ -11,23 +11,21 @@ sacli.Command.new("uploadObjects")
   .action(async (args) => {
     const startTime = process.hrtime.bigint();
     const data = Buffer.from("DEADBEEF".repeat(Math.ceil(args.size / 8)).slice(0, args.size));
-    const poolMgr = Array.from({length: args.concurrency}, () => new TurbostoreClient({host: "127.0.0.1", port: 9000, onSocketError: console.error}));
-    const poolWkr = Array.from({length: args.concurrency}, () => new TurbostoreClient({host: "127.0.0.1", port: 9001, onSocketError: console.error}));
+    const pool = Array.from({length: args.concurrency}, () => new TurbostoreClient({host: "127.0.0.1", port: 9001}));
     const wg = waitGroup();
     for (let no = 0; no < args.count; no++) {
       wg.add();
-      const connMgr = poolMgr[no % poolMgr.length];
-      const connWkr = poolWkr[no % poolWkr.length];
+      const conn = pool[no % pool.length];
       const k = key(no);
       (async () => {
         let phase;
         try {
           phase = "create";
-          const {objectNumber} = await connMgr.createObject(k, args.size);
+          const {inodeDeviceOffset, objectNumber} = await conn.createObject(k, args.size);
           phase = "write";
-          await connWkr.writeObjectWithBuffer(k, objectNumber, 0, data);
+          await conn.writeObjectWithBuffer(inodeDeviceOffset, objectNumber, 0, data);
           phase = "commit";
-          await connMgr.commitObject(k, objectNumber);
+          await conn.commitObject(inodeDeviceOffset, objectNumber);
         } catch (err) {
           console.error(`Failed to upload ${k} (in phase ${phase}): ${err.message}`);
         }
@@ -39,7 +37,6 @@ sacli.Command.new("uploadObjects")
     console.log(`Effective time: ${effectiveSec} seconds`);
     console.log(`Effective processing rate: ${args.count / effectiveSec} per second`);
     console.log(`Effective bandwidth: ${data.length * args.count / 1024 / 1024 / effectiveSec} MiB/s`);
-    await Promise.all(poolMgr.map(conn => conn.close()));
-    await Promise.all(poolWkr.map(conn => conn.close()));
+    await Promise.all(pool.map(conn => conn.close()));
   })
   .eval(process.argv.slice(2));
